565cb1db3756429b53dba33432567bf1b605c755
Taleiko Anton V <taleiko@cubbli22-gold.local>
2023-10-05 15:58:19 +0300

    Processing of byte strings before the dataframe is created

--- info_extraxter.py
+++ info_extraxter.py
@@ -3,11 +3,11 @@ import pandas as pd
 import hdf5_getters as g
 
 # Real
-MSD_ROOT = "/home/taleiko/Documents/Introduction to data science/Mini-project/MillionSongSubset"
+# MSD_ROOT = "/home/taleiko/Documents/Introduction to data science/Mini-project/MillionSongSubset"
 # Subfolder for testing
-# MSD_ROOT = "/home/taleiko/Documents/Introduction to data science/Mini-project/MillionSongSubset/B/I"
+MSD_ROOT = "/home/taleiko/Documents/Introduction to data science/Mini-project/MillionSongSubset/B/I"
 OUTPUT_FILE = "data/song_info.csv"
-# OUTPUT_FILE = "data/song_info_test.csv"
+OUTPUT_FILE = "data/song_info_test.csv"
 
 files = []
 
@@ -26,14 +26,14 @@ def createSongInfoCsv(filePaths):
     for filePath in filePaths:
         h5 = g.open_h5_file_read(filePath)
         row = [
-            g.get_song_id(h5),
-            g.get_title(h5),
-            g.get_artist_name(h5),
+            g.get_song_id(h5).decode(),
+            g.get_title(h5).decode(),
+            g.get_artist_name(h5).decode(),
             # How do we use the terms as a feature?
             # Are they consistent or improvised for each song?
             # If improvised, then they are just useless noise.
             # g.get_artist_terms(h5),
-            g.get_artist_location(h5),
+            g.get_artist_location(h5).decode(),
             # Meaning unclear
             # g.get_danceability(h5),
             g.get_tempo(h5),
@@ -53,6 +53,8 @@ def createSongInfoCsv(filePaths):
                                      "year"
                                     ])
     # print(df)
+    for c in ["song_id", "title", "artist_name", "location"]:
+        df[c] = df[c].str.strip("b'")
     df.to_csv(OUTPUT_FILE, index=False)
 
 
