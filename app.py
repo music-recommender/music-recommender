61546ea581078fd0a06828d022e4b84346f1720d
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2023-10-07 15:09:06 +0300

    Improvements for Tabulator
    Selected rows are now frozen below the headers so that selected songs stay at sight even if e.g. page is changed or user scrolls down on page. Also, added a reset button to clear Tabulator's selection, frozen columns and outputs.

--- app.py
+++ app.py
@@ -42,31 +42,45 @@ filters = {
         "placeholder": "Enter year"},
 }
 
+msd_df = pd.read_csv(INPUT_FILE)
 tab = pn.widgets.Tabulator(
-    pd.read_csv(INPUT_FILE),
-    pagination="remote",
+    msd_df,
+    pagination="local",
     layout="fit_columns",
     page_size=10,
     sizing_mode="stretch_width",
     header_filters=filters,
-    selectable="checkbox"
+    disabled=True,
+    selectable='checkbox',
+    frozen_rows=[]
 )
 
-run_button = pn.widgets.Button(name='\u25b6', width=100, height=100)
+output_column = pn.Column()
+run_button = pn.widgets.Button(icon='run', button_type='primary', name='Run', width=100)
+reset_button = pn.widgets.Button(icon='trash', button_type='danger', name='Reset', width=100)
 
-@pn.depends(b=run_button)
+@pn.depends(s=tab.param.selection, watch=True)
+def selection_callback(s):
+    tab.frozen_rows = s
+
+@pn.depends(r=reset_button, watch=True)
+def reset(r):
+    output_column.clear()
+    tab.selection = []
+    tab.value = msd_df
+
+
+@pn.depends(b=run_button, watch=True)
 def run(b):
-    if len(tab.selection) == 0:
-        return
-    return tab.value.iloc[tab.selection]
+    return output_column
 
 template = pn.template.VanillaTemplate(title="Music Recommender", sidebar=[])
 template.main.append(
     pn.Column(
         "# Songs",
         tab,
-        run_button,
-        run
+        pn.Row(run_button, reset_button),
+        output_column
     )
 )
 template.servable()
