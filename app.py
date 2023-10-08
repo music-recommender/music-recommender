89c5eba58d28c4d181399e50fd3ef5ed7cee078b
Matias Tamsi <55825941+matiastamsi@users.noreply.github.com>
2023-10-08 23:26:30 +0300

    Refactoring and changes...
    Changed functionality so that when a user selects a song then automativally recommendations are calculated and outputs are showed. See that currently selecting more than one song produces nothing because giving multiple songs for the model is not yet implemented. These changes led to major refactoring and app.py is much simpler yet more easy and efficient.

--- app.py
+++ app.py
@@ -7,40 +7,17 @@ pn.extension("tabulator")
 INPUT_FILE = "data/song_info_complete_rows.csv"
 
 filters = {
-    "title": {
-        "type": "input",
-        "func": "like",
-        "placeholder": "Enter title"
-    },
-    "artist_name": {
-        "type": "input",
-        "func": "like",
-        "placeholder": "Enter artist"
-    },
-    "artist_terms": {
-        "type": "input",
-        "func": "like",
-        "placeholder": "Enter terms"
-    },
-    "location": {
-        "type": "input",
-        "func": "like",
-        "placeholder": "Enter location"
-    },
+    "title": {"type": "input", "func": "like", "placeholder": "Enter title"},
+    "artist_name": {"type": "input", "func": "like", "placeholder": "Enter artist"},
+    "artist_terms": {"type": "input", "func": "like", "placeholder": "Enter terms"},
+    "location": {"type": "input", "func": "like", "placeholder": "Enter location"},
     "danceability": {
         "type": "number",
         "func": ">=",
         "placeholder": "Enter minimum danceability",
     },
-    "tempo": {
-        "type": "number",
-        "func": ">=",
-        "placeholder": "Enter minimum tempo"
-    },
-    "year": {
-        "type": "number",
-        "func": "=",
-        "placeholder": "Enter year"},
+    "tempo": {"type": "number", "func": ">=", "placeholder": "Enter minimum tempo"},
+    "year": {"type": "number", "func": "=", "placeholder": "Enter year"},
 }
 
 msd_df = pd.read_csv(INPUT_FILE)
@@ -52,37 +29,26 @@ tab = pn.widgets.Tabulator(
     sizing_mode="stretch_width",
     header_filters=filters,
     disabled=True,
-    selectable='toggle'
+    selectable="toggle",
 )
 
-output_column = pn.Column()
-run_button = pn.widgets.Button(icon='run', button_type='primary', name='Run', width=100)
-reset_button = pn.widgets.Button(icon='trash', button_type='danger', name='Reset', width=100)
 
-@pn.depends(s=tab.param.selection, watch=True)
-def selection_callback(s):
-    tab.frozen_rows = s
+@pn.depends(s=tab.param.selection)
+def output(s):
+    if len(s) == 1:
+        return pn.Column(
+            "# Recommendations",
+            pn.widgets.Tabulator(
+                tab.value.iloc[recommendSongs(s[0])[0]],
+                sizing_mode="stretch_width",
+                layout="fit_columns",
+                disabled=True,
+            ),
+        )
+    else:
+        return
 
-@pn.depends(r=reset_button, watch=True)
-def reset(r):
-    output_column.clear()
-    tab.selection = []
-    tab.value = msd_df
-
-
-@pn.depends(b=run_button, watch=True)
-def run(b):
-    output_column.append(tab.value.iloc[recommendSongs(tab.selection[0])[0]])
-
-    return output_column
 
 template = pn.template.VanillaTemplate(title="Music Recommender", sidebar=[])
-template.main.append(
-    pn.Column(
-        "# Songs",
-        tab,
-        pn.Row(run_button, reset_button),
-        output_column
-    )
-)
+template.main.append(pn.Column("# Songs", tab, output))
 template.servable()
