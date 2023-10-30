f8ff5e0453521afb1ec87b2b7c615c6775bfe909
Taleiko Anton V <taleiko@cubbli22-gold.local>
2023-10-30 15:18:45 +0200

    Code cleaning

--- music_recommender.py
+++ music_recommender.py
@@ -1,16 +1,3 @@
-# TODO
-# VERIFICATION DATA
-# Take jam_to_msd.tsv and
-#   create maps for the needed conversions
-# Take likes.tsv and
-#   group the dataset by user IDs
-#   pick a user and check if the recommended songs are in the set of liked songs
-#   PROBLEM: How do we choose what user to pick?
-#     If the user is picked randomly, then the correct answer is not consistent
-#     every time the model is trained.
-#     sort the user IDs and pick the first one? Then it is always the same for
-#     each set of data.
-
 LIKES_FILE = "data/likes.tsv"
 JAM_MSD_FILE = "data/jam_to_msd.tsv"
 SONGS_FILE = "data/song_info_complete_rows.csv"
@@ -51,15 +38,3 @@ def recommendSongs(selection, k, cols, songs):
     )
 
     return songs_copy.iloc[v[0][1:]]
-
-# Can this be removed?
-# Verifying results with Thisismyjam
-# dfJam_msd = pd.read_csv(JAM_MSD_FILE, sep="\t", names=["jam_id", "msd_id"])
-# print(dfJam_msd)
-# # jamToMsd = {row["jam_id"]: row["msd_id"] for row in df.iloc}
-
-# df_likes = pd.read_csv(LIKES_FILE, sep="\t")
-# print(df_likes)
-# sorted_likes = df_likes.sort_values("jam_id")
-# print(sorted_likes)
-# likes_groups = df_likes.groupby("user_id")
